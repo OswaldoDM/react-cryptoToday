450509e64cf1d54595301a6471cdae8234338bca
Oswaldo <oswaldoe1992@gmail.com>
2022-10-27 13:17:35 -0400

    last update

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
   
 );
 
